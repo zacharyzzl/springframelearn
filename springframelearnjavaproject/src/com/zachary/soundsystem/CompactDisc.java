108013886d257e0136e02434ec0e2acf0cb03a20
zacharyzzl <1015989118@qq.com>
2020-06-20 21:25:28 +0800

    创建可被发现的bean

---
+++ springframelearnjavaproject/src/com/zachary/soundsystem/CompactDisc.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.zachary.soundsystem;
+
+/**
+ * @author Zachary.Zheng
+ * @version 1.0
+ * @date 2020年6月20日 下午9:20:07
+ */
+public interface CompactDisc {
+	void play();
+}
