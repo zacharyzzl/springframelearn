9dec41b77f962523c70670a346d4d2fee2983ba1
zacharyzzl <1015989118@qq.com>
2020-06-21 17:01:52 +0800

    为bean测试做准备

--- springframelearnjavaproject/src.main.java/com/zachary/soundsystem/impl/CDPlayer.java
+++ springframelearnjavaproject/src.main.java/com/zachary/soundsystem/impl/CDPlayer.java
@@ -1,8 +1,5 @@
 package com.zachary.soundsystem.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import com.zachary.soundsystem.CompactDisc;
 import com.zachary.soundsystem.MediaPlayer;
 
@@ -11,28 +8,15 @@ import com.zachary.soundsystem.MediaPlayer;
  * @version 1.0
  * @date 2020年6月21日 下午3:17:35
  */
-@Component
 public class CDPlayer implements MediaPlayer {
-
 	private CompactDisc cd;
-
-//	@Autowired
-//	public CDPlayer(CompactDisc cd) {
-//		this.cd = cd;
-//	}
-
-//	@Autowired
-//	public void setCd(CompactDisc cd) {
-//		this.cd = cd;
-//	}
-
-	@Autowired
-	public void insertCd(CompactDisc cd) {
-		this.cd = cd;
-	}
 
 	@Override
 	public void play() {
 		cd.play();
+	}
+
+	public void setCd(CompactDisc cd) {
+		this.cd = cd;
 	}
 }
