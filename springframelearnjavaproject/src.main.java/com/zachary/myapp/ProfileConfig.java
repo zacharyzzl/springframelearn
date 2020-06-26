9ecf666b86edcad04c9a8aba45f2d7e254d21c09
zacharyzzl <1015989118@qq.com>
2020-06-26 20:27:14 +0800

    Spring3.2开始，可以在方法上使用@Profile注解测试

--- springframelearnjavaproject/src.main.java/com/zachary/myapp/ProfileConfig.java
+++ springframelearnjavaproject/src.main.java/com/zachary/myapp/ProfileConfig.java
@@ -3,8 +3,15 @@
 */  
 package com.zachary.myapp;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
+
+import com.zachary.soundsystem.impl.BlankDisc;
 
 /**  
 * @author Zachary.Zheng 
@@ -12,7 +19,28 @@ import org.springframework.context.annotation.Import;
 * @date 2020年6月26日  
 */
 @Configuration
-@Import({DevelopmentProfileConfig.class, ProductionProfileConfig.class})
+//@Import({DevelopmentProfileConfig.class, ProductionProfileConfig.class})
 public class ProfileConfig {
-
+	
+	@Bean("blankDisc")
+	@Profile("dev")
+	public BlankDisc getDevBlankDisc() {
+		BlankDisc blankDisc = new BlankDisc();
+		blankDisc.setTitle("Sgt. Pepper's Lonely Hearts Club Band prod");
+		List<String> tracks = new ArrayList<>();
+		tracks.add("Sgt. Pepper's Lonely Hearts Club Band track prod");
+		blankDisc.setTracks(tracks);
+		return blankDisc;
+	}
+	
+	@Bean("blankDisc")
+	@Profile("prod")
+	public BlankDisc getProdBlankDisc() {
+		BlankDisc blankDisc = new BlankDisc();
+		blankDisc.setTitle("Sgt. Pepper's Lonely Hearts Club Band prod");
+		List<String> tracks = new ArrayList<>();
+		tracks.add("Sgt. Pepper's Lonely Hearts Club Band track prod");
+		blankDisc.setTracks(tracks);
+		return blankDisc;
+	}
 }
