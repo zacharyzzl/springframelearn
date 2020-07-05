3cca9f11d2ae7ff9221a38f4336c800ce5519b15
zacharyzzl <1015989118@qq.com>
2020-07-05 17:21:10 +0800

    xml配置，环绕测试

--- springframelearnjavaproject/src.main.java/com/zachary/aop/Audience.java
+++ springframelearnjavaproject/src.main.java/com/zachary/aop/Audience.java
@@ -3,10 +3,9 @@
 */  
 package com.zachary.aop;
 
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 
 /**  
@@ -19,23 +18,16 @@ public class Audience {
 	@Pointcut("execution(* com.zachary.aop.Performance.perform(..))")
 	public void performance() {};
 	
-	@Before("performance()")
-	public void silenceCellPhones() {
-		System.out.println("Sliencing cell phones");
-	}
-	
-	@Before("performance()")
-	public void takeSeats() {
-		System.out.println("Taking seats");
-	}
-	
-	@AfterReturning("performance()")
-	public void applause() {
-		System.out.println("CLAP CLAP CLAP!!!");
-	}
-	
-	@AfterThrowing("performance()")
-	public void demandRefund() {
-		System.out.println("Demand a refund");
+	@Around("performance()")
+	public void watchPerformance(ProceedingJoinPoint jp) {
+		try {
+			System.out.println("Sliencing cell phones");
+			System.out.println("Taking seats");
+			jp.proceed();
+			System.out.println("CLAP CLAP CLAP!!!");
+			System.out.println("Demand a refund");
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
 	}
 }
