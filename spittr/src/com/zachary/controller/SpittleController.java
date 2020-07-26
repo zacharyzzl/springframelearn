18abaafe195156fd66cbd3f997bc57fca1225760
zacharyzzl <1015989118@qq.com>
2020-07-26 11:07:26 +0800

    @RequestParam测试

--- spittr/src/com/zachary/controller/SpittleController.java
+++ spittr/src/com/zachary/controller/SpittleController.java
@@ -5,20 +5,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.zachary.data.SpittleRepository;
 
 @Controller
 @RequestMapping("/spittles")
 public class SpittleController {
-	
 	private SpittleRepository spittleRepository;
-	
+
 	@RequestMapping(method = RequestMethod.GET)
-	public String spittles(Model model) {
-		model.addAttribute("spittleList", spittleRepository.findSpittles(100, 20));
+	public String spittles(@RequestParam(value = "max", defaultValue = "1000") long max,
+			@RequestParam(value = "count", defaultValue = "20") int count, Model model) {
+		model.addAttribute("spittleList", spittleRepository.findSpittles(max, count));
 		return "spittles";
 	}
+
 	@Autowired
 	public SpittleController(SpittleRepository spittleRepository) {
 		super();
