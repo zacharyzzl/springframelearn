136c713b9163e6d7dc3962624dccbb1e5544425a
zacharyzzl <1015989118@qq.com>
2020-07-26 11:11:49 +0800

    @PathVariable测试

--- spittr/src/com/zachary/controller/SpittleController.java
+++ spittr/src/com/zachary/controller/SpittleController.java
@@ -3,6 +3,7 @@ package com.zachary.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,6 +19,12 @@ public class SpittleController {
 	public String spittles(@RequestParam(value = "max", defaultValue = "1000") long max,
 			@RequestParam(value = "count", defaultValue = "20") int count, Model model) {
 		model.addAttribute("spittleList", spittleRepository.findSpittles(max, count));
+		return "spittles";
+	}
+
+	@RequestMapping(value="/{count}", method = RequestMethod.GET)
+	public String spittle(@PathVariable("count") int count, Model model) {
+		model.addAttribute("spittleList", spittleRepository.findSpittles(Long.MAX_VALUE, count));
 		return "spittles";
 	}
 
