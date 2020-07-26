763dd61ebcf5deedccdacc906bddd2bac6f5a122
zacharyzzl <1015989118@qq.com>
2020-07-26 20:18:58 +0800

    编写@PathVariable和处理表单的控制类

---
+++ spittr/WebContent/WEB-INF/views/registerForm.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Spittle</title>
+</head>
+<body>
+	<h1>Register</h1>
+	
+	<form method="post">
+		First Name: <input type="text" name="firstName" />
+		Last Name: <input type="text" name="lastName" />
+		Username: <input type="text" name="username" />
+		Password: <input type="text" name="password" />
+		<input type="submit" value="Register" />
+	</form>
+</body>
+</html>
--- spittr/src/com/zachary/controller/SpittleController.java
+++ spittr/src/com/zachary/controller/SpittleController.java
@@ -1,14 +1,18 @@
 package com.zachary.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.zachary.data.SpittleRepository;
+import com.zachary.vo.Spittle;
 
 @Controller
 @RequestMapping("/spittles")
@@ -22,12 +26,34 @@ public class SpittleController {
 		return "spittles";
 	}
 
-	@RequestMapping(value="/{count}", method = RequestMethod.GET)
+	@RequestMapping(value="/spittle/{count}", method = RequestMethod.GET)
 	public String spittle(@PathVariable("count") int count, Model model) {
 		model.addAttribute("spittleList", spittleRepository.findSpittles(Long.MAX_VALUE, count));
 		return "spittles";
 	}
-
+	
+	@RequestMapping(value="/register", method = RequestMethod.GET)
+	public String showRegistrationForm() {
+		return "registerForm";
+	}
+	
+	@RequestMapping(value="/register", method = RequestMethod.POST)
+	public String processRegistration(@Valid Spittle spittle, Errors errors) {
+		System.out.println(spittle);
+		if(errors.hasErrors()) {
+			return "registerForm";
+		}
+		return "redirect:/spittles/processRegistration/" + spittle.getUsername();
+	}
+	
+	@RequestMapping(value="/processRegistration/{username}", method = RequestMethod.GET)
+	public String processRegistration(@PathVariable("username") String username, Model model) {
+		Spittle spittle = new Spittle();
+		spittle.setUsername(username);
+		model.addAttribute(spittle);
+		return "profile";
+	}
+	
 	@Autowired
 	public SpittleController(SpittleRepository spittleRepository) {
 		super();
